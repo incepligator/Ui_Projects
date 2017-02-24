c2b02e520715a2f2040b5f8c320f450e957b36d0
Shrawan <incepligator@gmail.com>
2017-02-23 18:28:41 -0600

    Day 1 html and Day 2 html with css and html desc

---
+++ Projects1/Script/curve.js
@@ -0,0 +1,26 @@
+"use strict"
+
+function curve(){
+    
+    var a= 2;
+    var b= 3;
+    return a+b;
+    
+}
+console.log("sample javascript file");
+console.log(curve());
+
+for(var i=0; i<10; i++){
+    
+    console.log(i);
+    
+}
+
+console.log("this will give the exact value form the loop");
+console.log("value i = "+i);
+ for (let j=0; j<10; j++){
+     
+     console.log(j);
+     
+ }
+//console.log("value o j=" +j);
